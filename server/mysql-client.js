2d1c89463175cabc9111aee9a8b6d6429fb94cb5
Hwanggyuyoung <famous90h@gmail.com>
2015-12-07 12:29:15 +0900

    tighten security

--- server/mysql-client.js
+++ server/mysql-client.js
@@ -1,27 +1,22 @@
 var mysql = require('mysql');
+var Passport = require('./passport');
 
 // local DB for test
-var client = mysql.createConnection({
-    user: 'root',
-    password: 'q1w2e3r4',
-    database: 'bank'
-});
+//var client = mysql.createConnection({
+//    user: 'root',
+//    password: 'q1w2e3r4',
+//    database: 'bank'
+//});
 
 // AWS RDS
-var rdsEndpoint = {
-    host: 'problembankdbinstance.cu3kda75wdql.ap-northeast-1.rds.amazonaws.com',
-    database: 'problemBankDB',
-    user: 'bankAdmin',
-    password: 'q1w2e3r4',
-    port: 3306
-};
+var rdsEndpoint = Passport.rdsTicket;
 
-//var client = mysql.createConnection({
-//    host: rdsEndpoint.host,
-//    user: rdsEndpoint.user,
-//    password: rdsEndpoint.password,
-//    port: rdsEndpoint.port,
-//    database: rdsEndpoint.database
-//});
+var client = mysql.createConnection({
+    host: rdsEndpoint.host,
+    user: rdsEndpoint.user,
+    password: rdsEndpoint.password,
+    port: rdsEndpoint.port,
+    database: rdsEndpoint.database
+});
 
 module.exports = client;
